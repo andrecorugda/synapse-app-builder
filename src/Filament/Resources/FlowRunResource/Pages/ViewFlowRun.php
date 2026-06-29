85d5e44c1383c6edfbbaa53b63c4a9a4fd929b8b
andrecorugda <andre.alarcon.corugda@gmail.com>
2026-06-29 17:19:24 +0800

    feat(flow): read-only Flow Runs resource with a step inspector

---
+++ src/Filament/Resources/FlowRunResource/Pages/ViewFlowRun.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Andre\AiPageBuilder\Filament\Resources\FlowRunResource\Pages;
+
+use Andre\AiPageBuilder\Filament\Resources\FlowRunResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewFlowRun extends ViewRecord
+{
+    protected static string $resource = FlowRunResource::class;
+}
